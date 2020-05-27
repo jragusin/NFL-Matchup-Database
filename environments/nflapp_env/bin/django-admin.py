364f86329016d9cdc2d8b242b4b8ce8d499ba978
Joshua Ragusin <jragusin@utexas.edu>
2020-05-27 01:24:27 -0500

    Fixed an issue with the environments/ file structure

---
+++ environments/nflapp_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/raggy/Desktop/nfl_application/NFL-Matchup-Database/environments/nflapp_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
